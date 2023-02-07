57e17eb88ad6b6e78fce5b6a5b0e8c9e67282ddd
RealityMoez <122297651+RealityMoez@users.noreply.github.com>
2023-02-07 04:15:04 +0200

    Update script.js
    -some enhancements.
    -reduce code redundancy.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-/* VARIABLES */
-var page_start = true;
+/* ----------------------------------------------VARIABLES----------------------------------------- */
+let pageStart = true;
 const header = document.getElementsByClassName("header")[0];
 const logo = document.getElementsByClassName("logo")[0];
 const logoLink = document.getElementsByTagName("a")[0];
 
-const nav = document.getElementsByClassName("navigation");
-const navLinks = nav[0].querySelectorAll("ul a");
+const nav = document.getElementsByClassName("navigation")[0];
+const navLinks = nav.querySelectorAll("ul a");
 
 const styleSheet = document.styleSheets[0];
 
-const containerTitle_selector = '.container-title::after';
-const containerTitle_idx = Array.from(styleSheet.cssRules).findIndex(rule => rule.selectorText === containerTitle_selector);
+const sectionTitle_selector = '.container-title::after';
+const AfterSectionTitleIndex = Array.from(styleSheet.cssRules).findIndex(rule => rule.selectorText === sectionTitle_selector);
 
-const cardBorder_selector = '.card';
-const cardBorder_idx = Array.from(styleSheet.cssRules).findIndex(rule => rule.selectorText === cardBorder_selector);
+const cardSelector = '.card';
+const cardIndex = Array.from(styleSheet.cssRules).findIndex(rule => rule.selectorText === cardSelector);
 
 const navIndicator = document.querySelector(".indicator");
 
 const sections = document.querySelectorAll('section');
 const windowHeight = window.innerHeight;
-/* VARIABLES END */
+/* ----------------------------------------------VARIABLES END----------------------------------------- */
 
 
 // SCROLLING ANIMATION
@@ -28,13 +28,9 @@ const observer = new IntersectionObserver((entries) =>
 	entries.forEach((entry) =>
 	{
 		if (entry.isIntersecting)
-		{
 			entry.target.classList.add("show");
-		}
 		else
-		{
 			entry.target.classList.remove("show");
-		}
 	});
 });
 
@@ -43,121 +39,76 @@ hidden_elements.forEach((element) => observer.observe(element));
 // ─────────────────────────────────────────────────────────────────────────────
 
 
-// CHANGE ELEMENT COLOR BASED ON EVENT
-function color_change()
+// CHANGE ELEMENTS' COLOR & POSITION BASED ON EVENT
+function updateOnEvent()
 {
-	refresh();
+	window.addEventListener('scroll', updateColors);
+	window.addEventListener('scroll', navIndicator_windowScroll);
 
-	window.addEventListener('scroll', () => 
+	logo.addEventListener("mouseover", updateColors);
+	logo.addEventListener("mouseout", updateColors);
+
+	for (const nav of navLinks)
 	{
-		if (window.scrollY <= 180)
-		{
-			page_start = true;
-			header.style.setProperty('transition', 'background-color 0.3s ease');
-			header.style.backgroundColor = "transparent";
-
-			updateColors();
-		}
-		else
-		{
-			page_start = false;
-			header.style.setProperty('transition', 'background-color 0.3s ease');
-			header.style.backgroundColor = "var(--light-blue-color)";
-
-			updateColors();
-		}
-	});
-
-	function refresh()
-	{
-		logo.addEventListener("mouseover", updateColors);
-		logo.addEventListener("mouseout", updateColors);
-
-		for (const nav of navLinks)
-		{
-			nav.addEventListener("mouseover", updateColors);
-			nav.addEventListener("mouseout", updateColors);
-		}
+		nav.addEventListener("mouseover", updateColors);
+		nav.addEventListener("mouseout", updateColors);
 	}
 
 	function updateColors()
 	{
-		if (page_start)
+		pageStart = (window.scrollY <= 180);
+
+		if (pageStart)
 		{
-			header.style.setProperty('transition', 'background-color 0.5s ease');
+			header.style.transition = 'background-color 0.5s ease';
 			header.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
 
-			logoLink.style.color = "var(--white-color)";
+			if (logo.matches(":hover"))
+				logoLink.style.color = "aqua";
+			else
+				logoLink.style.color = "white";
 
 			for (const nav of navLinks)
 			{
-				nav.style.setProperty('transition', '0.2s ease');
-				nav.style.color = "var(--yellow-color)";
+				nav.style.transition = '0.2s ease';
+				nav.style.color = "yellow";
 				nav.style.backgroundColor = "transparent";
 
 				if (nav.matches(":hover"))
 				{
-					nav.style.backgroundColor = "var(--white-color)";
-					nav.style.color = "var(--blue-color)";
+					nav.style.backgroundColor = "white";
+					nav.style.color = "blue";
 				}
 			}
-			if (logo.matches(":hover"))
-			{
-				logoLink.style.color = "var(--light-blue-color)";
-			}
-
 		}
 		else
 		{
-			header.style.setProperty('transition', 'background-color 2s ease');
-			header.style.backgroundColor = "var(--light-blue-color)";
+			header.style.transition = 'background-color 2s ease';
+			header.style.backgroundColor = "aqua";
 
-			logoLink.style.color = "var(--black-color)";
+			logoLink.style.color = "black";
 
-			styleSheet.cssRules[containerTitle_idx].style.backgroundColor = 'aqua';
-
-			styleSheet.cssRules[cardBorder_idx].style.setProperty('border', '3px solid aqua');
-
-			const updateNavIndicator = (target) =>
-			{
-				if (navIndicator.style.left == `${target.offsetLeft}px`)
-				{
-					navIndicator.style.backgroundColor = 'white';
-				}
-				else
-				{
-					navIndicator.style.backgroundColor = 'black';
-				}
-			};
+			styleSheet.cssRules[AfterSectionTitleIndex].style.backgroundColor = 'aqua';
+			styleSheet.cssRules[cardIndex].style.border = '3px solid aqua';
 
 			for (const nav of navLinks)
 			{
-				nav.addEventListener("mouseover", () =>
-				{
-					updateNavIndicator(nav);
-					clickedLink = null;
-				});
 				nav.addEventListener("mouseout", () =>
 				{
-					navIndicator.style.backgroundColor = 'black';
-					clickedLink = null;
+					navIndicator.style.transition = '0.3s ease-in-out width, 0.3s ease left, 0.3s ease top, 1s ease background-color';
+					navIndicator_windowScroll();
 				});
-				nav.addEventListener("click", () => updateNavIndicator(nav));
-			}
+				nav.addEventListener("mouseover", navIndicator_windowScroll);
 
-			for (const nav of navLinks)
-			{
-				nav.style.setProperty('transition', '0.2s ease');
-
-				nav.style.color = "var(--black-color)";
-
+				nav.style.transition = '0.2s ease';
+				nav.style.color = "black";
 				nav.style.backgroundColor = "transparent";
 
 				if (nav.matches(":hover"))
 				{
-					nav.style.backgroundColor = "var(--black-color)";
-					nav.style.color = "var(--white-color)";
-					nav.style.setProperty('transition', 'background-color 0.4s ease, color 0.4s ease');
+					nav.style.backgroundColor = "black";
+					nav.style.color = "white";
+					nav.style.transition = 'background-color 0.4s ease, color 0.4s ease';
 				}
 			}
 
@@ -165,27 +116,28 @@ function color_change()
 			{
 				logoLink.style.color = "white";
 				header.style.backgroundColor = "black";
-				header.style.setProperty('transition', 'background-color 2s ease');
+				header.style.transition = 'background-color 2s ease';
 
 				navIndicator.style.backgroundColor = 'white';
 
 				for (const nav of navLinks)
 				{
-					nav.style.setProperty('transition', '2s ease');
+					nav.style.transition = '2s ease';
 					nav.style.color = 'yellow';
 				}
 
-				styleSheet.cssRules[containerTitle_idx].style.setProperty('transition', 'background-color 2s ease');
-				styleSheet.cssRules[containerTitle_idx].style.backgroundColor = 'yellow';
+				styleSheet.cssRules[AfterSectionTitleIndex].style.transition = 'background-color 2s ease';
+				styleSheet.cssRules[AfterSectionTitleIndex].style.backgroundColor = 'yellow';
 
-				styleSheet.cssRules[cardBorder_idx].style.setProperty('transition', 'border-color 2s ease');
-				styleSheet.cssRules[cardBorder_idx].style.setProperty('border', '3px solid yellow');
+				styleSheet.cssRules[cardIndex].style.transition = 'border-color 2s ease';
+				styleSheet.cssRules[cardIndex].style.border = '3px solid yellow';
 			}
-			logo.addEventListener("mouseout", () => (navIndicator.style.backgroundColor = 'black'));
+
+			logo.addEventListener("mouseout", () => { navIndicator.style.backgroundColor = 'black'; });
 		}
 	}
 }
-color_change();
+updateOnEvent();
 // ─────────────────────────────────────────────────────────────────────────────
 
 
@@ -206,56 +158,46 @@ var type = new Typed(".changing_text", {
 });
 // ─────────────────────────────────────────────────────────────────────────────
 
-
-/* NAVIGATION INDICATOR */
-let clickedLink = null;
-
-function nav_indicate()
-{
-	navLinks.forEach(link =>
-	{
-		link.addEventListener('click', e =>
-		{
-			clickedLink = link;
-			navIndicator.style.width = `${link.offsetWidth}px`;
-			navIndicator.style.left = `${link.offsetLeft}px`;
-		});
-	});
-}
+/* ----------------------------------------------NAVIGATION INDICATOR----------------------------------------- */
 
 // TODO: NAVIGATION INDICATOR AUTO SCROLL BASED ON SECTION
 function navIndicator_windowScroll()
 {
 	if (window.scrollY < 250)
-	{
 		navIndicator.style.width = '0';
-	}
-	else if (clickedLink)
-	{
-		navIndicator.style.width = `${clickedLink.offsetWidth}px`;
-	}
 
-	const scrollTop = window.scrollY;
+	const scrollTop = document.documentElement.scrollTop;
+	const scrollBottom = document.documentElement.scrollTop + windowHeight;
+
 	sections.forEach(function (section, i)
 	{
-		if (section.offsetTop < scrollTop + windowHeight / 2 && scrollTop < section.offsetTop + windowHeight / 2)
+		if (i > 0)
 		{
-			for (const nav of navLinks)
-			{
-				if (section.hasAttribute('id') && nav.getAttribute("href") == '#' + section.id)
-				{
-					if (!clickedLink)
-					{
-						navIndicator.style.width = `${nav.offsetWidth}px`;
-						navIndicator.style.left = `${nav.offsetLeft}px`;
+			const sectionTop = section.offsetTop;
+			const sectionBottom = section.offsetTop + section.offsetHeight;
 
+			if (scrollTop >= sectionTop - 150 && sectionBottom > scrollBottom - 200) 
+			{
+				for (const nav of navLinks)
+				{
+					if (section.hasAttribute('id') && nav.getAttribute("href") == '#' + section.id)
+					{
+						// FOR SMALLER WINDOW WIDTH
+						navIndicator.style.top = `${nav.offsetTop + nav.offsetHeight - 8}px`;
+						// ─────────────────────────────────────────────────────────────────────────────
 						if (nav.matches(":hover"))
 						{
 							navIndicator.style.backgroundColor = 'white';
+							navIndicator.style.width = `${nav.offsetWidth}px`;
+							navIndicator.style.left = `${nav.offsetLeft}px`;
 						}
-						else
+						else if (!nav.matches(":hover"))
 						{
-							navIndicator.style.backgroundColor = 'black';
+							if (!logo.matches(":hover"))
+								navIndicator.style.backgroundColor = 'black';
+
+							navIndicator.style.width = `${nav.offsetWidth - 24}px`;
+							navIndicator.style.left = `${nav.offsetLeft + 12}px`;
 						}
 
 					}
@@ -265,7 +207,4 @@ function navIndicator_windowScroll()
 	});
 }
 // ─────────────────────────────────────────────────────────────────────────────
-
-nav_indicate();
-window.addEventListener('scroll', navIndicator_windowScroll);
-/* NAVIGATION INDICATOR END */
+/* ----------------------------------------------NAVIGATION INDICATOR END----------------------------------------- */
